ea7a1d1c0187cdcabb95b581f0eba17b3fb312ad
BasuDev <adbasudev54@gmail.com>
2019-09-01 20:00:52 +0545

    video call ui upgraded

--- wwwroot/js/makecall.js
+++ wwwroot/js/makecall.js
@@ -1,5 +1,13 @@
 ﻿"use strict";
-setTimeout(Call, 100);
+
+$(document).ready(function () {
+    connectionStart();
+    Call();
+});
+
+var state = 0;
+
+
 var connection = new signalR.HubConnectionBuilder()
     .withUrl("/message", {
         accessTokenFactory: () => "testing"
@@ -12,54 +20,57 @@ $("#button").click(function () {
 connection.on("connect_by_receiver", function () {
     Call();
 });
+navigator.mediaDevices.getUserMedia({ video: true }).then(stream =>
+    document.getElementById('mero_video').srcObject = new MediaStream(stream.getVideoTracks()));
 
 const to = $("#user").attr("Value");
 const video = document.getElementById('usko_video');
-
 const peerconnection = new RTCPeerConnection();
-peerconnection.createOffer()
-   .then(sdp =>console.log(sdp))
-
-
-function Call() {  
-
+async function Call() {
     navigator.mediaDevices.getUserMedia({ video: true, audio: true }).
-        then(function (stream) {
-            createOffer();
-            peerconnection.addStream(stream);
-            document.getElementById('mero_video').srcObject = new MediaStream(stream.getVideoTracks());
-            function createOffer() {
-                peerconnection.createOffer()
-                    .then(function (sdp) {
-                        console.log("mysdp:  "+sdp)
-                        peerconnection.setLocalDescription(sdp);
-                        connection.invoke("Call_Request", to, peerconnection.localDescription);
-                    })
-                    .catch(function (error) {
-                        console.log("errors: " +error)
-                    }
-                );
-        }
+        then( function (stream) {
+             peerconnection.addStream(stream);
+           /* document.getElementById('mero_video').srcObject = new MediaStream(stream.getVideoTracks());*/
+            peerconnection.createOffer()
+                .then(sdp => peerconnection.setLocalDescription(sdp))
+                .then(function () {
+                    connection.invoke('Call_Request', to, peerconnection.localDescription);
+                    //console.log(peerconnection.localDescription);
+                });
+            peerconnection.onaddstream = function (event) {
+                video.srcObject = event.stream;
+                state = 1;
+                console.log(state);
+            };
+            if (state == 0) {
+                setTimeout(Call, 500);
+
+            };
         });
-    peerconnection.onaddstream = function (event) {
-        video.srcObject = event.stream;
-
+ 
+    connection.on('call_accepted', function (_sdp) {
+        console.log(_sdp);
+        peerconnection.setRemoteDescription(_sdp);
     }
-    connection.on('call_accepted', function (sdp) {
-        console.log("utabata aako sdp: "+sdp);
-        peerconnection.setRemoteDescription(sdp);  
+    );  
+}
+
+
+
+
+
+async function connectionStart() {
+    try {
+        await connection.start();
+        console.log("connected");
+    } catch (err) {
+        console.log(err);
+        setTimeout(() => connectionStart(), 5000);
     }
-    );
-    console.log("connectionState  " + peerconnection.connectionState);  
-  
-}; 
+};
 
-
-connection.start().then(function () {
-}).catch(function (err) {
-
-    return console.error(err.toString());
-
+connection.onclose(async () => {
+    await connectionStart();
 });
 
     
